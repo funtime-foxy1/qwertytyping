a3244ebefe71bb07738b8968a7516beb5481a7c0
FunFoxRR <dldilley01@gmail.com>
2025-01-10 19:30:12 +0000

    hi

--- qwertytyping/src/app/components.js
+++ qwertytyping/src/app/components.js
@@ -1,28 +1,37 @@
-export function TopNav() {
+// import Link from "next/link";
+// import { Router } from "next/router";
+
+export function TopNav({ onSwitch }) {
     return (
-        <div id="topnav" className="fixed top-0 left-0 w-screen bg-container p-4 px-8">
-            <h1>
-                <div>
-                    <div>
-                        q                        
+        <div id="topnav" className="flex justify-between items-center fixed top-0 left-0 w-screen bg-container p-4 px-8">
+            <h1 className="flex gap-2 items-center">
+                <div className="flex gap-1">
+                    <div className="letter_title">
+                        q
                     </div>
-                    <div>
-                        q                        
+                    <div className="letter_title">
+                        w
                     </div>
-                    <div>
-                        q                        
+                    <div className="letter_title">
+                        e
                     </div>
-                    <div>
-                        q                        
+                    <div className="letter_title">
+                        r
                     </div>
-                    <div>
-                        q                        
+                    <div className="letter_title">
+                        t
                     </div>
-                    <div>
-                                 y            
+                    <div className="letter_title">
+                        y
                     </div>
                 </div>
+                <span>typing</span>
             </h1>
+            <div className="flex gap-1 items-center text-lg">
+                <a onClick={() => onSwitch("about")} href="#" className="no-underline hover:underline">about</a>
+                <div className="size-1 bg-white rounded-full opacity-35 mx-1"></div>
+                <a onClick={() => onSwitch("links")} href="#" className="no-underline hover:underline">links</a>
+            </div>
         </div>
     );
 }
--- qwertytyping/src/app/page.js
+++ qwertytyping/src/app/page.js
@@ -1,9 +1,30 @@
+'use client'
+
 import { TopNav } from "./components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 export default function Home() {
+    // const router = useRouter();
+    // const [] = useState();
+
+    const onswitch = (tab) => {
+        console.log("onSwitch:", tab);
+        router.push("/" + tab, { scroll: true });
+    };
     return (
-        <div id="root">
-            <TopNav />
+        <div>
+            <TopNav onSwitch={(tab) => onswitch(tab)} />
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/">
+                        <Route>
+
+                        </Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
