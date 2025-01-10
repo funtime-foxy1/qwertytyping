6564cd4af02c9fca39534dd6fbb78e3206f77fd6
FunFoxRR <dldilley01@gmail.com>
2025-01-10 19:29:51 +0000

    Change, i dont know

---
+++ qwertytyping/src/app/components.js
@@ -0,0 +1,28 @@
+export function TopNav() {
+    return (
+        <div id="topnav" className="fixed top-0 left-0 w-screen bg-container p-4 px-8">
+            <h1>
+                <div>
+                    <div>
+                        q                        
+                    </div>
+                    <div>
+                        q                        
+                    </div>
+                    <div>
+                        q                        
+                    </div>
+                    <div>
+                        q                        
+                    </div>
+                    <div>
+                        q                        
+                    </div>
+                    <div>
+                                 y            
+                    </div>
+                </div>
+            </h1>
+        </div>
+    );
+}
---
+++ qwertytyping/src/app/page.js
@@ -0,0 +1,9 @@
+import { TopNav } from "./components";
+
+export default function Home() {
+    return (
+        <div id="root">
+            <TopNav />
+        </div>
+    );
+}
